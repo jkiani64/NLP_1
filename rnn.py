df15d1eddaa474972bb81d4f2c48f3cb3eff0172
jkiani64 <jalalkianii@gmail.com>
2021-01-28 20:23:57 -0600

    add runn_step_forward

--- rnn.py
+++ rnn.py
@@ -24,6 +24,14 @@ def clip(gradients, maxValue):
     
     return gradients
 
+def rnn_step_forward(parameters, a_prev, x):
+    
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+    a_next = np.tanh(np.dot(Wax, x) + np.dot(Waa, a_prev) + b) # hidden state
+    p_t = softmax(np.dot(Wya, a_next) + by) # unnormalized log probabilities for next chars # probabilities for next chars 
+    
+    return a_next, p_t
+
 def sample(parameters, char_to_ix, seed):
     """
     Sample a sequence of characters according to a sequence of probability distributions output of the RNN
