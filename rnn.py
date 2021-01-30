4879e8fedf08192d60de5e2f03a05a3e81decb57
jkiani64 <jalalkianii@gmail.com>
2021-01-30 17:31:10 -0600

    Move all functions to rnn.py

--- rnn.py
+++ rnn.py
@@ -1,6 +1,290 @@
 import numpy as np
 from rnn_utils import *
 
+
+def rnn_cell_forward(xt, a_prev, parameters):
+    """
+    Implements a single forward step of the RNN-cell
+
+    Arguments:
+    xt -- your input data at timestep "t", numpy array of shape (n_x, m).
+    a_prev -- Hidden state at timestep "t-1", numpy array of shape (n_a, m)
+    parameters -- python dictionary containing:
+                        Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
+                        Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
+                        Waa -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        ba --  Bias, numpy array of shape (n_a, 1)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+    Returns:
+    a_next -- next hidden state, of shape (n_a, m)
+    yt_pred -- prediction at timestep "t", numpy array of shape (n_y, m)
+    cache -- tuple of values needed for the backward pass, contains (a_next, a_prev, xt, parameters)
+    """
+    
+    # Retrieve parameters from "parameters"
+    Wax =  parameters["Wax"]
+    Waa =  parameters["Waa"]
+    Wya =  parameters["Wya"]
+    ba =  parameters["ba"]
+    by =  parameters["by"]
+    
+    
+    # Compute the next activation
+    a_next = np.tanh(np.dot(Wax, xt) + np.dot(Waa, a_prev) + ba)
+    
+    
+    # Compute prediction at time step "t"
+    yt_pred = softmax(np.dot(Wya, a_next) + by)
+    
+    
+    # tuple of values needed for the backward pass
+    cache = (a_next, a_prev, xt, parameters)
+    
+    return a_next, yt_pred, cache
+
+
+def rnn_forward(x, a0, parameters):
+    """
+    Implement the forward propagation of the recurrent neural network described in Figure (3).
+
+    Arguments:
+    x -- Input data for every time-step, of shape (n_x, m, T_x).
+    a0 -- Initial hidden state, of shape (n_a, m)
+    parameters -- python dictionary containing:
+                        Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
+                        Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
+                        Wya -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        ba --  Bias numpy array of shape (n_a, 1)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+
+    Returns:
+    a -- Hidden states for every time-step, numpy array of shape (n_a, m, T_x)
+    y_pred -- Predictions for every time-step, numpy array of shape (n_y, m, T_x)
+    caches -- tuple of values needed for the backward pass, contains (list of caches, x)
+    """
+    # Retrive the dimensions from shapes of x and parameters["Wya"]
+    n_x, m, T_x = x.shape
+    n_y, n_a = parameters['Wya'].shape
+    
+    # Initialize a which include the hidden states for every time-step
+    a = np.zeros((n_a, m , T_x))
+    
+    # Initialize y_pred which includes predictions for every time-step
+    y_pred = np.zeros((n_y, m , T_x))
+    
+    # Initialize caches which includes the list of caches 
+    caches = []
+    
+    
+    # Loop over all time steps
+    for t in range(T_x):
+        
+        # Retrive xt (input for the time step of t)
+        xt = x[:,:,t]
+        
+        #Update next hidden state, compute the prediction, get the cache
+        a[:,:,t], y_pred[:,:,t], cache = rnn_cell_forward(xt, a0, parameters)
+        
+        # Update a0
+        a0 = a[:,:,t]
+        
+        # Append "cache" to "caches"
+        caches.append(cache)
+        
+    # store values needed for backward propagation in cache
+    caches = (caches, x) 
+    
+    return a, y_pred, caches
+
+def lstm_cell_forward(xt, a_prev, c_prev, parameters):
+    """
+    Implement a single forward step of the LSTM-cell as described in Figure (4)
+
+    Arguments:
+    xt = your input data at timestep "t", numpy array of shape (n_x, m).
+    a_prev = Hidden state at timestep "t-1", numpy array of shape (n_a, m)
+    c_prev = Memory state at timestep "t-1", numpy array of shape (n_a, m)
+    parameters = python dictionary containing:
+                        Wf = Weight matrix of the forget gate, numpy array of shape (n_a, n_a + n_x)
+                        bf = Bias of the forget gate, numpy array of shape (n_a, 1)
+                        Wi = Weight matrix of the update gate, numpy array of shape (n_a, n_a + n_x)
+                        bi = Bias of the update gate, numpy array of shape (n_a, 1)
+                        Wc = Weight matrix of the first "tanh", numpy array of shape (n_a, n_a + n_x)
+                        bc =  Bias of the first "tanh", numpy array of shape (n_a, 1)
+                        Wo = Weight matrix of the output gate, numpy array of shape (n_a, n_a + n_x)
+                        bo =  Bias of the output gate, numpy array of shape (n_a, 1)
+                        Wy = Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        by = Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+                        
+    Returns:
+    a_next = next hidden state, of shape (n_a, m)
+    c_next = next memory state, of shape (n_a, m)
+    yt_pred = prediction at timestep "t", numpy array of shape (n_y, m)
+    cache = tuple of values needed for the backward pass, contains (a_next, c_next, a_prev, c_prev, xt, parameters)
+    
+    Note: ft/it/ot stand for the forget/update/output gates, cct stands for the candidate value (c tilde),
+          c stands for the memory value
+    """
+    
+    # Concate xt and a_prev
+    concat = np.concatenate((a_prev, xt), axis = 0)
+    
+    # Retrive the parameters
+    Wf = parameters['Wf']
+    bf = parameters['bf']
+    Wi = parameters['Wi']
+    bi = parameters['bi']
+    Wc = parameters['Wc']
+    bc = parameters['bc']
+    Wo = parameters['Wo']
+    bo = parameters['bo']
+    Wy = parameters['Wy']
+    by = parameters['by']
+    
+    # Calculate candidate value (c tilde)
+    cct = np.tanh(np.dot(Wc, concat) + bc)
+    
+    # Forget gate
+    ft = sigmoid(np.dot(Wf, concat) + bf)
+    
+    # Update gate
+    it = sigmoid(np.dot(Wi, concat) + bi)
+    
+    # Output gate
+    ot = sigmoid(np.dot(Wo, concat) + bo)
+    
+    # Compute next memory state
+    c_next = np.multiply(ft, c_prev) + np.multiply(it, cct)
+    
+    # Compute next hidden state
+    a_next = np.multiply(ot, np.tanh(c_next))
+    
+    # Compute yt_pred
+    yt_pred = softmax(np.dot(Wy, a_next) + by)
+    
+    
+    # values needed for the backward pass 
+    cache = (a_next, c_next, a_prev, c_prev, ft, it, cct, ot, xt, parameters)
+    
+    return a_next, c_next, yt_pred, cache
+
+
+def lstm_forward(x, a0, parameters):
+    """
+    Implement the forward propagation of the recurrent neural network using an LSTM-cell described in Figure (3).
+
+    Arguments:
+    x -- Input data for every time-step, of shape (n_x, m, T_x).
+    a0 -- Initial hidden state, of shape (n_a, m)
+    parameters -- python dictionary containing:
+                        Wf -- Weight matrix of the forget gate, numpy array of shape (n_a, n_a + n_x)
+                        bf -- Bias of the forget gate, numpy array of shape (n_a, 1)
+                        Wi -- Weight matrix of the update gate, numpy array of shape (n_a, n_a + n_x)
+                        bi -- Bias of the update gate, numpy array of shape (n_a, 1)
+                        Wc -- Weight matrix of the first "tanh", numpy array of shape (n_a, n_a + n_x)
+                        bc -- Bias of the first "tanh", numpy array of shape (n_a, 1)
+                        Wo -- Weight matrix of the output gate, numpy array of shape (n_a, n_a + n_x)
+                        bo -- Bias of the output gate, numpy array of shape (n_a, 1)
+                        Wy -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+                        
+    Returns:
+    a -- Hidden states for every time-step, numpy array of shape (n_a, m, T_x)
+    y -- Predictions for every time-step, numpy array of shape (n_y, m, T_x)
+    caches -- tuple of values needed for the backward pass, contains (list of all the caches, x)
+    """
+    # Initialize chaches
+    caches = []
+    
+    # Retrive the dimmensions
+    (n_x, m, T_x) = x.shape
+    Wy = parameters['Wy']
+    (n_y, n_a) = Wy.shape
+    
+    
+    # Initialize a
+    a = np.zeros((n_a, m, T_x))
+    c = np.zeros((n_a, m, T_x))
+    
+    # Initialize y
+    y = np.zeros((n_y, m, T_x))
+    
+    # Initialize c0
+    
+    c0 = np.zeros((n_a, m))
+    
+    # Loop over different time step
+    for t in range(T_x):
+        
+        # Forward single pass
+        a_next, c_next, yt_pred, cache = lstm_cell_forward(x[:,:,t], a0, c0, parameters)
+        
+        # Store a_next
+        a[:,:,t] = a_next
+        
+        # Store c_next
+        c[:,:,t] = c_next
+        
+        # Store yt_pred
+        y[:,:,t] = yt_pred
+        
+        
+        # Append cache
+        caches.append(cache)
+        
+        
+        # Update a0 and x_prev
+        a0 = a_next
+        c0 = c_next
+        
+    caches = (caches, x)
+    
+    
+    return a, y, c,  caches
+
+
+def rnn_cell_backward(da_next, cache):
+    """
+    Implements the backward pass for the RNN-cell (single time-step).
+
+    Arguments:
+    da_next -- Gradient of loss with respect to next hidden state
+    cache -- python dictionary containing useful values (output of rnn_cell_forward())
+
+    Returns:
+    gradients -- python dictionary containing:
+                        dx -- Gradients of input data, of shape (n_x, m)
+                        da_prev -- Gradients of previous hidden state, of shape (n_a, m)
+                        dWax -- Gradients of input-to-hidden weights, of shape (n_a, n_x)
+                        dWaa -- Gradients of hidden-to-hidden weights, of shape (n_a, n_a)
+                        dba -- Gradients of bias vector, of shape (n_a, 1)
+    """
+    # Retrive the information
+    a_next, a_prev, xt, parameters = cache
+    
+    # Retrieve parameters from "parameters"
+    Wax =  parameters["Wax"]
+    Waa =  parameters["Waa"]
+    Waa =  parameters["Waa"]
+    ba =  parameters["ba"]
+    by =  parameters["by"]
+    
+    dtanh = np.multiply(1 - np.square(np.tanh(np.dot(Wax, xt) + np.dot(Waa, a_prev) + ba)), da_next)
+    
+    dxt = np.dot(Wax.T, dtanh)
+    
+    
+    da_prev = np.dot(Waa.T, dtanh)
+    dWax = np.dot(dtanh, xt.T)
+    dWaa = np.dot(dtanh, a_prev.T)
+    dba = np.sum(dtanh, axis = 1).reshape(-1, 1)
+    
+    
+    gradients = {'dxt':dxt, 'da_prev':da_prev, 'dWax':dWax, 'dWaa':dWaa, 'dba':dba}
+    
+    
+    return gradients
+
 def clip(gradients, maxValue):
     '''
     Clips the gradients' values between minimum and maximum.
@@ -32,34 +316,34 @@ def rnn_step_forward(parameters, a_prev, x):
     
     return a_next, p_t
 
-def rnn_forward(X, Y, a_prev, parameters, vocab_size = 27):
-    """ Performs the forward propagation through the RNN and computes the cross-entropy loss.
-    It returns the loss' value as well as a "cache" storing values to be used in the backpropagation.
-    """
-    # Initialize x, a, and y_hat as empty dictionaries
-    x, a, y_hat = {}, {}, {}
+# def rnn_forward(X, Y, a_prev, parameters, vocab_size = 27):
+#     """ Performs the forward propagation through the RNN and computes the cross-entropy loss.
+#     It returns the loss' value as well as a "cache" storing values to be used in the backpropagation.
+#     """
+#     # Initialize x, a, and y_hat as empty dictionaries
+#     x, a, y_hat = {}, {}, {}
     
-    a[-1] = np.copy(a_prev)
+#     a[-1] = np.copy(a_prev)
     
-    # Initialize the loss to 0
-    loss = 0 
+#     # Initialize the loss to 0
+#     loss = 0 
     
-    for t in range(len(X)):
+#     for t in range(len(X)):
         
-        # Set x[t] to be the one-hot vector representation of the t'th character in X.
-        x[t] = np.zeros((vocab_size,1)) 
-        if (X[t] != None):
-            x[t][X[t]] = 1
+#         # Set x[t] to be the one-hot vector representation of the t'th character in X.
+#         x[t] = np.zeros((vocab_size,1)) 
+#         if (X[t] != None):
+#             x[t][X[t]] = 1
             
-        # Run one step forward of the RNN
-        a[t], y_hat[t] = rnn_step_forward(parameters, a[t-1], x[t])
+#         # Run one step forward of the RNN
+#         a[t], y_hat[t] = rnn_step_forward(parameters, a[t-1], x[t])
         
-        # Update the loss by substracting the cross-entropy term of this time-step from it.
-        loss -= np.log(y_hat[t][Y[t], 0])
+#         # Update the loss by substracting the cross-entropy term of this time-step from it.
+#         loss -= np.log(y_hat[t][Y[t], 0])
         
-    cache = (y_hat, a, x)
+#     cache = (y_hat, a, x)
         
-    return loss, cache
+#     return loss, cache
 
 
 def rnn_step_backward(dy, gradients, parameters, x, a, a_prev):
@@ -171,3 +455,48 @@ def sample(parameters, char_to_ix, seed):
         indices.append(char_to_ix['\n'])
           
     return indices
+
+def optimize(X, Y, a_prev, parameters, learning_rate = 0.01):
+    """
+    Execute one step of the optimization to train the model.
+    
+    Arguments:
+    X -- list of integers, where each integer is a number that maps to a character in the vocabulary.
+    Y -- list of integers, exactly the same as X but shifted one index to the left.
+    a_prev -- previous hidden state.
+    parameters -- python dictionary containing:
+                        Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
+                        Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
+                        Wya -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        b --  Bias, numpy array of shape (n_a, 1)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+    learning_rate -- learning rate for the model.
+    
+    Returns:
+    loss -- value of the loss function (cross-entropy)
+    gradients -- python dictionary containing:
+                        dWax -- Gradients of input-to-hidden weights, of shape (n_a, n_x)
+                        dWaa -- Gradients of hidden-to-hidden weights, of shape (n_a, n_a)
+                        dWya -- Gradients of hidden-to-output weights, of shape (n_y, n_a)
+                        db -- Gradients of bias vector, of shape (n_a, 1)
+                        dby -- Gradients of output bias vector, of shape (n_y, 1)
+    a[len(X)-1] -- the last hidden state, of shape (n_a, 1)
+    """
+    
+    ### START CODE HERE ###
+    
+    # Forward propagate through time (≈1 line)
+    loss, cache = rnn_forward(X, Y, a_prev, parameters)
+    
+    # Backpropagate through time (≈1 line)
+    gradients, a = rnn_backward(X, Y, parameters, cache)
+    
+    # Clip your gradients between -5 (min) and 5 (max) (≈1 line)
+    gradients = clip(gradients, 5)
+    
+    # Update parameters (≈1 line)
+    parameters = update_parameters(parameters, gradients, learning_rate)
+    
+    ### END CODE HERE ###
+    
+    return loss, gradients, a[len(X)-1]
