461de491c21c02dc4b52fdda22ddfbb2aad386c6
jkiani64 <jalalkianii@gmail.com>
2021-01-31 16:30:42 -0600

    add model function, which train a rnn model and return the results and some helper functions as well

--- rnn.py
+++ rnn.py
@@ -726,3 +726,82 @@ def initialize_parameters(n_a, n_x, n_y, seed = 1):
 
 def get_initial_loss(vocab_size, seq_length):
     return -np.log(1.0/vocab_size)*seq_length
+
+def smooth(loss, cur_loss):
+    return loss * 0.999 + cur_loss * 0.001
+
+def print_sample(sample_ix, ix_to_char):
+    txt = ''.join(ix_to_char[ix] for ix in sample_ix)
+    txt = txt[0].upper() + txt[1:]  # capitalize first character 
+    print ('%s' % (txt, ), end='')
+    
+def model(data, ix_to_char, char_to_ix, num_iterations = 35000, n_a = 50, dino_names = 7, vocab_size = 27):
+    """
+    Trains the model and generates dinosaur names. 
+    
+    Arguments:
+    data -- text corpus
+    ix_to_char -- dictionary that maps the index to a character
+    char_to_ix -- dictionary that maps a character to an index
+    num_iterations -- number of iterations to train the model for
+    n_a -- number of units of the RNN cell
+    dino_names -- number of dinosaur names you want to sample at each iteration. 
+    vocab_size -- number of unique characters found in the text, size of the vocabulary
+    
+    Returns:
+    parameters -- learned parameters
+    """
+    
+    # Retrieve n_x and n_y from vocab_size
+    n_x, n_y = vocab_size, vocab_size
+    
+    # Initialize parameters
+    parameters = initialize_parameters(n_a, n_x, n_y)
+    
+    # Initialize loss (this is required because we want to smooth our loss)
+    loss = get_initial_loss(vocab_size, dino_names)
+    
+    # Build list of all dinosaur names (training examples).
+    with open("data/dinos.txt") as f:
+        examples = f.readlines()
+    
+    examples = [e.lower().strip() for e in examples]
+    # Shuffle list of all dinosaur names
+    np.random.seed(0)
+    np.random.shuffle(examples)
+    
+    # Initialize the hidden state of LSTM
+    a_prev = np.zeros((n_a, 1))
+    
+    # Optimization loop
+    for j in range(num_iterations):
+        
+        # TO define an example
+        index = j % len(examples)
+        X = [None] + [char_to_ix[ch] for ch in examples[index]]
+        Y = X[1:] + [char_to_ix["\n"]]
+        
+        # Perform one optimization step: Forward-prop -> Backward-prop -> Clip -> Update parameters
+        # Choose a learning rate of 0.01
+        curr_loss, gradients, a_prev = optimize(X, Y, a_prev, parameters)
+        
+        # Use a latency trick to keep the loss smooth. It happens here to accelerate the training.
+        loss = smooth(loss, curr_loss)
+        
+        # Every 2000 Iteration, generate "n" characters thanks to sample() to check if the model is learning properly
+        if j % 2000 == 0:
+            
+            print('Iteration: %d, Loss: %f' % (j, loss) + '\n')
+            
+            # The number of dinosaur names to print
+            seed = 0
+            for name in range(dino_names):
+                
+                # Sample indices and print them
+                sampled_indices = sample(parameters, char_to_ix, seed)
+                print_sample(sampled_indices, ix_to_char)
+                
+                seed += 1  # To get the same result for grading purposed, increment the seed by one. 
+            print('\n')
+    
+    return parameters
