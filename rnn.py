187d94ffa8097acb8f470f750af9affe474706f8
jkiani64 <jalalkianii@gmail.com>
2021-01-30 19:44:41 -0600

    add lstm_cell_backward to rnn.py

--- rnn.py
+++ rnn.py
@@ -343,6 +343,74 @@ def rnn_backward(da, caches):
 
     return gradients
 
+
+
+def lstm_cell_backward(da_next, dc_next, cache):
+    """
+    Implement the backward pass for the LSTM-cell (single time-step).
+
+    Arguments:
+    da_next -- Gradients of next hidden state, of shape (n_a, m)
+    dc_next -- Gradients of next cell state, of shape (n_a, m)
+    cache -- cache storing information from the forward pass
+
+    Returns:
+    gradients -- python dictionary containing:
+                        dxt -- Gradient of input data at time-step t, of shape (n_x, m)
+                        da_prev -- Gradient w.r.t. the previous hidden state, numpy array of shape (n_a, m)
+                        dc_prev -- Gradient w.r.t. the previous memory state, of shape (n_a, m, T_x)
+                        dWf -- Gradient w.r.t. the weight matrix of the forget gate, numpy array of shape (n_a, n_a + n_x)
+                        dWi -- Gradient w.r.t. the weight matrix of the update gate, numpy array of shape (n_a, n_a + n_x)
+                        dWc -- Gradient w.r.t. the weight matrix of the memory gate, numpy array of shape (n_a, n_a + n_x)
+                        dWo -- Gradient w.r.t. the weight matrix of the output gate, numpy array of shape (n_a, n_a + n_x)
+                        dbf -- Gradient w.r.t. biases of the forget gate, of shape (n_a, 1)
+                        dbi -- Gradient w.r.t. biases of the update gate, of shape (n_a, 1)
+                        dbc -- Gradient w.r.t. biases of the memory gate, of shape (n_a, 1)
+                        dbo -- Gradient w.r.t. biases of the output gate, of shape (n_a, 1)
+    """
+
+    # Retrieve information from "cache"
+    (a_next, c_next, a_prev, c_prev, ft, it, cct, ot, xt, parameters) = cache
+    
+    
+    # Retrieve dimensions from xt's and a_next's shape
+    (n_x, m) = xt.shape
+    (n_a, m) = a_next.shape
+    
+    # Compute gates related derivatives, you can find their values can be 
+    # found by looking carefully at equations (7) to (10)
+    dot  = da_next * np.tanh(c_next) * ot * (1-ot)
+    dcct = (da_next * ot * (1- np.square(np.tanh(c_next)))  + dc_next) * it * (1-cct**2)
+    dit  = (da_next * ot * (1 - np.tanh(c_next) ** 2) + dc_next) * cct * (1 - it) * it
+    dft  = (da_next * ot * (1 - np.tanh(c_next) ** 2) + dc_next) * c_prev * ft * (1 - ft)
+    
+    
+    # Compute parameters related derivatives. Use equations (11)-(14)
+    concat = np.vstack([a_prev, xt])
+    dWf = np.dot(dft, concat.T)
+    dWi = np.dot(dit, concat.T)
+    dWc = np.dot(dcct, concat.T)
+    dWo = np.dot(dot, concat.T)
+    dbf = np.sum(dft, axis = 1 , keepdims=True)
+    dbi = np.sum(dit, axis = 1 , keepdims=True)
+    dbc = np.sum(dcct, axis = 1 , keepdims=True)
+    dbo = np.sum(dot, axis = 1 , keepdims=True)
+    
+    
+    # Compute derivatives w.r.t previous hidden state, previous memory state and input. Use equations (15)-(17)
+    da_prev = np.dot(Wf[:, :n_a].T, dft) + np.dot(Wc[:, :n_a].T, dcct) + np.dot(Wi[:, :n_a].T, dit) + np.dot(Wo[:, :n_a].T, dot)
+    dc_prev = (da_next * ot * (1 - np.tanh(c_next) ** 2) + dc_next) * ft
+    dxt = np.dot(Wf[:, n_a:].T, dft) + np.dot(Wc[:, n_a:].T, dcct) + np.dot(Wi[:, n_a:].T, dit) + np.dot(Wo[:, n_a:].T, dot)
+    
+    
+    # Save gradients in dictionary
+    gradients = {"dxt": dxt, "da_prev": da_prev, "dc_prev": dc_prev, "dWf": dWf,"dbf": dbf, "dWi": dWi,"dbi": dbi,
+                "dWc": dWc,"dbc": dbc, "dWo": dWo,"dbo": dbo}
+    
+    
+    return gradients
+
+
 def clip(gradients, maxValue):
     '''
     Clips the gradients' values between minimum and maximum.
