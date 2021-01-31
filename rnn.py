569a042290daf5feb215589f6e234494fe4d2e1c
jkiani64 <jalalkianii@gmail.com>
2021-01-31 11:33:20 -0600

    uncomment rnn_backward_character and rnn_step_backward

--- rnn.py
+++ rnn.py
@@ -551,41 +551,39 @@ def rnn_forward_character(X, Y, a_prev, parameters, vocab_size = 27):
     return loss, cache
 
 
-# def rnn_step_backward(dy, gradients, parameters, x, a, a_prev):
+def rnn_step_backward(dy, gradients, parameters, x, a, a_prev):
     
-#     gradients['dWya'] += np.dot(dy, a.T)
-#     gradients['dby'] += dy
-#     da = np.dot(parameters['Wya'].T, dy) + gradients['da_next'] # backprop into h
-#     daraw = (1 - a * a) * da # backprop through tanh nonlinearity
-#     gradients['db'] += daraw
-#     gradients['dWax'] += np.dot(daraw, x.T)
-#     gradients['dWaa'] += np.dot(daraw, a_prev.T)
-#     gradients['da_next'] = np.dot(parameters['Waa'].T, daraw)
-#     return gradients
+    gradients['dWya'] += np.dot(dy, a.T)
+    gradients['dby'] += dy
+    da = np.dot(parameters['Wya'].T, dy) + gradients['da_next'] # backprop into h
+    daraw = (1 - a * a) * da # backprop through tanh nonlinearity
+    gradients['db'] += daraw
+    gradients['dWax'] += np.dot(daraw, x.T)
+    gradients['dWaa'] += np.dot(daraw, a_prev.T)
+    gradients['da_next'] = np.dot(parameters['Waa'].T, daraw)
+    return gradients
 
 
-# def rnn_backward(X, Y, parameters, cache):
-#     # Initialize gradients as an empty dictionary
-#     gradients = {}
+def rnn_backward_character(X, Y, parameters, cache):
+    # Initialize gradients as an empty dictionary
+    gradients = {}
     
-#     # Retrieve from cache and parameters
-#     (y_hat, a, x) = cache
-#     Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+    # Retrieve from cache and parameters
+    (y_hat, a, x) = cache
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
     
-#     # each one should be initialized to zeros of the same dimension as its corresponding parameter
-#     gradients['dWax'], gradients['dWaa'], gradients['dWya'] = np.zeros_like(Wax), np.zeros_like(Waa), np.zeros_like(Wya)
-#     gradients['db'], gradients['dby'] = np.zeros_like(b), np.zeros_like(by)
-#     gradients['da_next'] = np.zeros_like(a[0])
+    # each one should be initialized to zeros of the same dimension as its corresponding parameter
+    gradients['dWax'], gradients['dWaa'], gradients['dWya'] = np.zeros_like(Wax), np.zeros_like(Waa), np.zeros_like(Wya)
+    gradients['db'], gradients['dby'] = np.zeros_like(b), np.zeros_like(by)
+    gradients['da_next'] = np.zeros_like(a[0])
     
-#     ### START CODE HERE ###
-#     # Backpropagate through time
-#     for t in reversed(range(len(X))):
-#         dy = np.copy(y_hat[t])
-#         dy[Y[t]] -= 1
-#         gradients = rnn_step_backward(dy, gradients, parameters, x[t], a[t], a[t-1])
-#     ### END CODE HERE ###
+    # Backpropagate through time
+    for t in reversed(range(len(X))):
+        dy = np.copy(y_hat[t])
+        dy[Y[t]] -= 1
+        gradients = rnn_step_backward(dy, gradients, parameters, x[t], a[t], a[t-1])
     
-#     return gradients, a
+    return gradients, a
 
 def update_parameters(parameters, gradients, lr):
 
